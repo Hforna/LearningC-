33a55e3fb230a6ffb4805f079dcd3100eee08482
Hforna <henriqueflashpingo@gmail.com>
2024-08-15 00:11:51 -0300

    Learning dates

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+
+namespace ConsoleApp1 {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("Write your birthday in the format dd/MM/yyyy");
+            string Input = Console.ReadLine();
+
+            DateTime d = DateTime.ParseExact(Input, "dd/MM/yyyy", CultureInfo.InvariantCulture);
+            DateTime now = DateTime.Now;
+
+            int age = now.Year - d.Year;
+            if (now < d.AddYears(age)) {
+                age--;
+            }
+
+            Console.WriteLine("You are " + age + " years old.");
+        }
+    }
+}
