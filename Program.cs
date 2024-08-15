e7689f5b5a76b25f6489fc37b25197ccc35ebab8
Hforna <henriqueflashpingo@gmail.com>
2024-08-15 00:19:32 -0300

    iso8691 in datetime

--- Program.cs
+++ Program.cs
@@ -9,7 +9,9 @@ namespace ConsoleApp1 {
 
             DateTime d = DateTime.ParseExact(Input, "dd/MM/yyyy", CultureInfo.InvariantCulture);
             DateTime now = DateTime.Now;
-
+            DateTime sec = new DateTime(2000, 11, 11, 21, 21, 34);
+            DateTime iso8691 = DateTime.Parse("2001/12/12T13:12:12Z");
+            Console.WriteLine(iso8691);
             int age = now.Year - d.Year;
             if (now < d.AddYears(age)) {
                 age--;
