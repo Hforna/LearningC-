4eb262dde95f6df30baf5fd5c37f5690240a531e
Hforna <henriqueflashpingo@gmail.com>
2024-08-17 17:56:53 -0300

    inheritance with bank accounts

--- Program.cs
+++ Program.cs
@@ -1,49 +1,65 @@
 ﻿using System;
 using System.Globalization;
-using System.Text;
 using ConsoleApp1;
 
 namespace ConsoleApp1
 {
-    class Comment
+    class AccountBank
     {
-        public string Text { get; set; }
-    }
-    class Post
-    {
-        public required string Title { get; set; }
-        public string Content { get; set; }
-        private DateTime Date = DateTime.Now;
-        public List<Comment> Comments = new List<Comment>();
+        public  int number {get; set;}
+        public required string holder { get; set; }
+        public double balance { get; private set; }
 
-        public void addComment(Comment comment)
+        public virtual void withDraw(double amount)
         {
-            Comments.Add(comment);
+            if (amount <= balance)
+            {
+                balance -= amount;
+            }
         }
 
-        public override string ToString()
+        public virtual void Deposit(double amount)
         {
-            StringBuilder s = new StringBuilder();
-            s.AppendLine(Title);
-            s.AppendLine(Content);
-            s.AppendLine(Date.ToString("dd/MM/yyyy HH/mm/ss"));
-            
-            s.AppendLine("Comments: ");
-            foreach(Comment c in Comments)
+            balance += amount;
+        }
+    }
+
+    class AccountBusiness : AccountBank
+    {
+        public double loanLimit = 2;
+
+        public void loan(double amount)
+        {
+            if (amount < loanLimit)
             {
-                s.AppendLine(c.Text);
+                Deposit(amount);
             }
-            return s.ToString();
+        }
+
+        public override void withDraw(double amount)
+        {
+            base.withDraw(amount);
+            base.withDraw(2);
         }
     }
     class Program
     {
         static void Main(string[] args)
         {
-            Comment newComment = new Comment {Text="Hello"};
-            Post newPost = new Post {Title="First book", Content="First Post on this page"};
-            newPost.addComment(newComment);
-            Console.WriteLine(newPost);
+            AccountBank account = new AccountBank { holder = "Henrique", number=200};
+            AccountBank s = new AccountBusiness { holder = "Henrique", number=400};
+            AccountBusiness account1 = (AccountBusiness) account;
+            account.Deposit(400);
+            account.withDraw(200);
+            s.Deposit(400);
+            s.withDraw(200);
+            Console.WriteLine(account.balance);
+            Console.WriteLine(s.balance);
+            if(s is AccountBank)
+            {
+                Console.WriteLine("s is account bank");
+            }
+
         }
     }
 }
