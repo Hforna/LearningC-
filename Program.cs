d95e8771f1a3ca39fcb1a491932e864f98bb7731
Hforna <henriqueflashpingo@gmail.com>
2024-08-18 20:52:15 -0300

    working with files

--- Program.cs
+++ Program.cs
@@ -1,73 +1,40 @@
 ﻿using System;
-using System.Globalization;
 using System.Collections.Generic;
-using ConsoleApp1;
+using System.IO;
 
-namespace ConsoleApp1
+public static class Solution {
+    
+}
+
+class Program
 {
-    class BankException : ApplicationException
+    static void Main(string[] args)
     {
-        public BankException(string message) : base(message)
+    FileStream file = new FileStream("../pj.csv", FileMode.Open); // add a file here
+    List<string> listfile = new List<string>();
+    using(StreamReader filest = new StreamReader(file))
+    {
+        while(!filest.EndOfStream)
         {
-
+            string fileline = filest.ReadLine();
+            listfile.Add(fileline);
         }
     }
-
-    class Account
+    Directory.CreateDirectory("out");
+    List<string> fin = new List<string>();
+    foreach(string list in listfile)
     {
-        public int Number { get; set; }
-        public string Holder { get; set; }
-        public double Balance { get; set; }
-        public double withdrawLimit { get; set; }
-
-        public Account()
+        var s = list.Split(", ");
+        double db = int.Parse(s[1].Trim()) * int.Parse(s[2].Trim());
+        fin.Add($"{s[0]}: {db}\n");
+    }
+    //Stream sss = File.Create("out/summary.csv");
+    using(StreamWriter write = File.AppendText("out/summary.csv"))
+    {
+        foreach(string line in fin)
         {
-
-        }
-
-        public void Deposit(double amount)
-        {
-            Balance += amount;
-        }
-
-        public void withdraw(double amount)
-        {
-            if(amount > Balance)
-            {
-                throw new BankException($"You just have ${Balance}, please digit a amount lower");
-            } else if(amount > withdrawLimit)
-            {
-                throw new BankException($"Your withdraw limit is {withdrawLimit}, please digit a amount lower than limit");
-            }
-
-            Balance -= amount;
+            write.WriteLine(line);
         }
     }
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Enter account data");
-            Console.Write("Number: "); 
-            int number = int.Parse(Console.ReadLine());
-            Console.Write("Holder: ");
-            string holder = Console.ReadLine(); 
-            Console.Write("Initial balance: ");
-            double balance = double.Parse(Console.ReadLine());
-            Console.Write("Withdraw limit: ");
-            double withdrawLimit = double.Parse(Console.ReadLine());
-            Console.Write("Enter amount for withdraw: ");
-            double withdraw = double.Parse(Console.ReadLine()); 
-            Account newAccount = new Account() {Balance=balance, Holder=holder, Number=number, withdrawLimit=withdrawLimit};
-            try
-            {
-                newAccount.withdraw(withdraw);
-                Console.WriteLine($"New balance: {newAccount.Balance}");
-            } catch(BankException e)
-            {
-                Console.WriteLine("Withdraw error" + e);
-            }
-        }
     }
 }
