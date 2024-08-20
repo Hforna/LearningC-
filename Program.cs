59b2523637000effadf5880720c42f8eb0e561f8
Hforna <henriqueflashpingo@gmail.com>
2024-08-19 22:00:02 -0300

    rent car project

--- Program.cs
+++ Program.cs
@@ -1,55 +1,93 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Text;
 
 
-class Tasks
+class BrazilTaxService
 {
-    public string Name { get; set; }
-    public DateTime Date = DateTime.Now;
-    public string Description;
-    private bool Complete = false;
+    public double Tax { get; set; }
+}
+class Vehicle
+{
+    public required string Name { get; set; }
 
-    public Tasks(string Name, string Description)
+    public Vehicle(string Name)
     {
         this.Name = Name;
-        this.Description = Description;
+    }
+}
+
+class RentaService
+{
+    public double pricePerhour { get; set; }
+    public double pricePerDay { get; set; }
+    private BrazilTaxService _brazilTaxService = new BrazilTaxService();
+
+    public void processInvoice(CarRental carRental)
+    {
+        TimeSpan duration = carRental.Finish.Subtract(carRental.Start);
+        double basicPayment;
+        if(duration.TotalHours <= 12)
+        {
+            basicPayment = pricePerhour * Math.Ceiling(duration.TotalHours);
+        } else {
+            basicPayment = pricePerDay * Math.Ceiling(duration.TotalDays);
+        }
+
+        _brazilTaxService.Tax = basicPayment;
+        carRental.invoice = new Invoice(basicPayment, _brazilTaxService.Tax);
+    }
+}
+
+class CarRental
+{
+    public DateTime Start;
+    public DateTime Finish;
+    public Vehicle vehicle;
+    public Invoice? invoice;
+
+    public CarRental(DateTime Start, DateTime Finish, Vehicle vehicle)
+    {
+        this.Start = Start;
+        this.Finish = Finish;
+        this.vehicle = vehicle;
+        this.invoice = null;
+    }
+}
+
+class Invoice
+{
+    public double basicPayment { private get; set; }
+    public double Tax { private get; set; }
+    
+    public Invoice(double basicPayment, double Tax)
+    {
+        this.basicPayment = basicPayment;
+        this.Tax = Tax;
+    }
+
+    public double TotalPayment
+    {
+        get { return basicPayment + Tax; }
     }
 
     public override string ToString()
     {
         StringBuilder sb = new StringBuilder();
-        sb.AppendLine(Name);
-        sb.AppendLine(Date.ToString());
-        sb.AppendLine(Description);
+        sb.AppendLine("Basic payment: " + basicPayment);
+        sb.AppendLine("Tax: " + Tax.ToString("F2", CultureInfo.InvariantCulture));
+        sb.AppendLine("Total payment: " + TotalPayment.ToString("F2", CultureInfo.InvariantCulture));
         return sb.ToString();
     }
-
-    public void CompleteTask()
-    {
-        Complete = true;
-    }
 }
+
+
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Tasks[] listTasks = new Tasks[6];
-        Directory.CreateDirectory("tasksdir");
-        for(int i = 1; i <= 6; i++)
-        {
-            listTasks[i] = new Tasks($"learn i in c#{i}", "Learn interfaces description in c#");     
-        }
-        FileStream fs = File.Create("tasksdir/taks.txt");
-        IEnumerable<Tasks> orderBy = listTasks.OrderBy(x => x.Date);
-        using(StreamWriter wr = new StreamWriter(fs))
-        {
-            foreach(Tasks task in listTasks)
-            {
-                wr.WriteLine(task);
-            }
-        }
 
     }
 }
