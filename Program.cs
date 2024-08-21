2860e0f81b6a3d087be2cbb85375b2b7352d78f1
Hforna <henriqueflashpingo@gmail.com>
2024-08-20 23:29:33 -0300

    SortedSet and HashSet

--- Program.cs
+++ Program.cs
@@ -3,64 +3,29 @@ using System.IO;
 using System.Collections.Generic;
 using System.Text;
 
-
-class Product : IComparable
-{
-    public string Name { get; set; }
-    public double Price { get; set; }
-
-    public Product(string name, double price)
-    {
-        Name = name;
-        Price = price;
-    }
-
-    public override string ToString()
-    {
-        return $"Name: {Name}, Price: {Price}";
-    }
-
-    public int CompareTo(object obj)
-    {
-        if (!(obj is Product))
-        {
-            throw new AggregateException("The object is not product");
-        }
-
-        Product otobject = obj as Product;
-        return Price.CompareTo(otobject.Price);
-    }
-
-}
-
-class CalcService
-{
-    public T Max<T>(List<T> list) where T : IComparable
-    {
-        T highest = list[0];
-        for (int i = 1; i < list.Count; i++)
-        {
-            if (list[i].CompareTo(highest) > 0)
-            {
-                highest = list[i];
-            }
-        }
-        return highest;
-    }
-}
-
-
-
 class Program
 {
     static void Main(string[] args)
     {
-        List<Product> list = new List<Product>();
-        for (int i = 0; i <= 10; i++)
+        HashSet<string> set = new HashSet<string>();
+
+        for(int i = 1; i <= 10; i++)
         {
-            list.Add(new Product($"Iphone {i}", 200 * i));
+            set.Add($"Henrique{i}");
         }
-        CalcService cl = new CalcService();
-        Console.WriteLine(cl.Max<Product>(list));
+        foreach(string name in set)
+        {
+            Console.WriteLine(name);
+        }
+
+        SortedSet<int> sortset = new SortedSet<int>() {1, 2, 20, 6, 8, 10};
+        SortedSet<int> sortsetd = new SortedSet<int>() { 1, 2, 212, 3, 558, 10 };
+        SortedSet<int> ssd = new SortedSet<int>(sortset);
+        ssd.IntersectWith(sortsetd);
+        foreach (int ss in ssd)
+        {
+            Console.WriteLine(ss);
+        }
+        Console.WriteLine(set.Contains("Henrique2"));
     }
 }
